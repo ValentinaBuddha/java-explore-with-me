798af7247e84a90da95b4dd06bfb77a8bf95dae8
Valentina <valentina.buddha@yandex.ru>
2023-07-07 16:18:59 +0300

    ewm stats

--- stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
+++ stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -18,19 +18,15 @@ public class EndpointHit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //Идентификатор сервиса для которого записывается информация
     @Column(nullable = false)
     private String app;
 
-    //URI для которого был осуществлен запрос
     @Column(nullable = false)
     private String uri;
 
-    //IP-адрес пользователя, осуществившего запрос
     @Column(nullable = false, length = 15)
     private String ip;
 
-    //Дата и время, когда был совершен запрос к эндпоинту
     @Column(nullable = false)
     private LocalDateTime timestamp;
 }
